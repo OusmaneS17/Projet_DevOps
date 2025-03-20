26a10ce4fcbd11f1d62e58c8adee787be8f31861
OusmaneS17 <oussoumanesow0@gmail.com>
2025-03-20 22:49:26 +0000

    reinstaller

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin 
-from django.urls import path, include 
-from . import views 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', views.Accueil_view, name = 'Accueil'),
-    path('Soumissions/', include('Soumissions.urls')),
-    path('inscription/', include('inscription.urls'))
-]
