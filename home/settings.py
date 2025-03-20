961ed9515f9f5d67bcf766ac01312b163eb3b540
OusmaneS17 <oussoumanesow0@gmail.com>
2025-03-20 12:36:18 +0000

    requierement

--- home/settings.py
+++ home/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'kssm',
         'USER': 'jenkins',
         'PASSWORD': 'jenkins_password',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
